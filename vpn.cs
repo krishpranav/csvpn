8ed2126e9df4cafe97b1b51676a5354efafa4b59
krishpranav <krisna.pranav@gmail.com>
2022-06-13 11:16:42 +0530

    vpn: add some func

--- vpn.cs
+++ vpn.cs
@@ -1,5 +1,23 @@
-﻿namespace vpn;
-public class Class1
-{
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
 
+namespace vpn {
+    public class VPN {
+        private const string EntryName = "VPN Connection";
+
+        private RasHandle handle = null;
+        private RasPhoneBook AllUsersPhoneBook;
+        public RasDialer Dialer;
+
+        public enum status { Default, Connection, Error, TimeOut }
+        public int VPN_Status { get; set; }
+
+        public void VPN_Connection(string VPN_Name, string VPN_ID, string VPN_PW) {
+            AllUsersPhoneBook = new RasPhoneBook();
+            AllUsersPhoneBook.Open();
+        }
+    }
 }
